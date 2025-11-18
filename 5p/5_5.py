9d399ddcec4ea5df490532335ab076bf6697ee5e
Maria Chepeleva <56710297+mrukly@users.noreply.github.com>
2025-11-18 08:50:49 +0700

    Update 5_5.py

--- 5p/5_5.py
+++ 5p/5_5.py
@@ -1,1 +1,20 @@
+weight = float(input()) 
+height = float(input()) 
 
+bmi = (weight / (height * height)) * (0.453592 / (0.0254 ** 2))
+bmi= round(bmi, 2)
+
+if bmi < 16:
+    print("выраженный дефицит массы тела")
+elif 16 <= bmi < 18.5:
+    print("недостаточная масса тела")
+elif 18.5 <= bmi < 25:
+    print("норма")
+elif 25 <= bmi < 30:
+    print("избыточная масса тела")
+elif 30 <= bmi < 35:
+    print("ожирение первой степени")
+elif 35 <= bmi < 40:
+    print("ожирение второй степени")
+else:
+    print("ожирение третьей степени")
