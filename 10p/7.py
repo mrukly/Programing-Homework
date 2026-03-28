e05f9dee8b4d501229e328a36324b3a9da898a4e
Maria Chepeleva <56710297+mrukly@users.noreply.github.com>
2026-03-28 11:48:33 +0700

    Update 7.py

--- 10p/7.py
+++ 10p/7.py
@@ -1,1 +1,10 @@
+def multiples(a, b, n):
+    for i in range(1, n + 1):
+        if i % a == 0 and i % b == 0:
+            print(i)
 
+a = int(input("A: "))
+b = int(input("B: "))
+n = int(input("N: "))
+
+multiples(a, b, n)
