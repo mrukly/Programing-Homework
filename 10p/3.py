e6933bb2038f7eb9e1e22b785dfdd897ec674548
Maria Chepeleva <56710297+mrukly@users.noreply.github.com>
2026-03-28 11:35:42 +0700

    Create 3.py

---
+++ 10p/3.py
@@ -0,0 +1,34 @@
+def price(price, card, holiday):
+    price = round(price, 2)
+    
+    discount = 0
+    
+    if price > 30000:
+        discount += 10
+    elif price > 20000:
+        discount += 7
+    elif price > 15000:
+        discount += 5
+    elif price > 5000:
+        discount += 3
+    
+    if card:
+        discount += 5
+    
+    if holiday:
+        discount += 3
+    
+    if discount > 15:
+        discount = 15
+    
+    price = price * (100 - discount) / 100
+    
+    price = round(price, 2)
+    
+    return price
+
+p = float(input("Стоимость: "))
+c = input("Есть ли карта?: ").lower() == "да"
+h = input("Сегодня праздник? ").lower() == "да"
+
+print(price(p, c, h))
