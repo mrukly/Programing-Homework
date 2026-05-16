41a33216b4c364c3cedebed263eeda7ac09a5097
Maria Chepeleva <56710297+mrukly@users.noreply.github.com>
2026-05-16 12:01:10 +0700

    Update 1.py

--- 8p/1.py
+++ 8p/1.py
@@ -1,1 +1,14 @@
+text = input()
 
+max_sp = 0
+cur_sp = 0
+
+for i in text:
+    if i.isspace():  
+        cur_sp += 1
+        if cur_sp > max_sp:
+            max_sp = cur_sp
+    else:
+        cur_sp = 0
+
+print(max_sp)
