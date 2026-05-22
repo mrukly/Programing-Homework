adb7bf2d0ee99153f9798535db137acd179dd24d
Maria Chepeleva <56710297+mrukly@users.noreply.github.com>
2026-05-23 01:41:04 +0700

    Update 1.py

--- 9p/1.py
+++ 9p/1.py
@@ -1,1 +1,7 @@
+with open('input.txt', 'r', encoding='utf-8') as file1:
+    data = file1.read()
 
+result = data.upper()
+
+with open('output.txt', 'w', encoding='utf-8') as file2:
+    file2.write(result)
