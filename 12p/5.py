60b34eb4f0d114ffb1923c1765a726662dbb3973
Maria Chepeleva <56710297+mrukly@users.noreply.github.com>
2026-05-23 10:13:00 +0700

    Create 5.py

---
+++ 12p/5.py
@@ -0,0 +1,22 @@
+def mod_number(a, b):
+    """
+    Finds the remainder when dividing natural number a by natural number b
+
+    Parameters:
+    a (int): The dividend (natural number, a >= 0)
+    b (int): The divisor (natural number, b >= 1)
+
+    Returns:
+    int: The remainder of a divided by b
+    """
+    if a < b:
+        return a
+    else:
+        return mod_number(a - b, b)
+
+
+a = int(input())
+b = int(input())
+
+result = mod_number(a, b)
+print(result)
