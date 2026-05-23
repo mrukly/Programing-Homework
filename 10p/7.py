59caa9e66d0483e35a0c31e84bc0b0cb6fa6c3b5
Maria Chepeleva <56710297+mrukly@users.noreply.github.com>
2026-05-23 15:27:43 +0700

    Update 7.py

--- 10p/7.py
+++ 10p/7.py
@@ -1,4 +1,15 @@
 def multiples(a, b, n):
+    """
+    Prints all common multiples of A and B up to N in ascending order
+
+    Parameters:
+    a (int): First natural number
+    b (int): Second natural number
+    n (int): Upper limit
+
+    Returns:
+    None
+    """
     for i in range(1, n + 1):
         if i % a == 0 and i % b == 0:
             print(i)
