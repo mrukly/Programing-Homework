f4bebf20254968998879ea81a8b37e65482d3f82
Maria Chepeleva <56710297+mrukly@users.noreply.github.com>
2026-05-23 10:09:08 +0700

    Create 3.py

---
+++ 12p/3.py
@@ -0,0 +1,24 @@
+def progress(a1, r, n):
+    """
+    Finds the n-th term of an arithmetic progression
+
+    Parameters:
+    a1 (float): The first term of the progression
+    r (float): The common difference
+    n (int): The position of the term to find (n >= 1)
+
+    Returns:
+    float: The n-th term of the arithmetic progression
+    """
+    if n == 1:
+        return a1
+    else:
+        return progress(a1, r, n - 1) + r
+
+
+a1 = float(input())
+r = float(input())
+n = int(input())
+
+result = progress(a1, r, n)
+print(result)
