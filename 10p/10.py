ee67b689851082498e4eb09e77d88a330f23d8c7
Maria Chepeleva <56710297+mrukly@users.noreply.github.com>
2026-05-23 15:32:57 +0700

    Update 10.py

--- 10p/10.py
+++ 10p/10.py
@@ -1,4 +1,15 @@
 def valid(A, B):
+    """
+    Prints numbers between A and B (inclusive) whose digits are only from {1,3,4,8,9}
+
+    Parameters:
+    A (int): Start of the range
+    B (int): End of the range
+
+    Returns:
+    None
+    """
+    
     if A > B:
         A, B = B, A
     for i in range(A, B + 1):
