6db821620ee871fa104301c7895df89c81aace02
Maria Chepeleva <56710297+mrukly@users.noreply.github.com>
2026-05-23 10:09:24 +0700

    Update 1.py

--- 12p/1.py
+++ 12p/1.py
@@ -1,13 +1,13 @@
 def pownum(a, n):
     """
-    Raises a real number a to a natural power n.
+    Raises a real number a to a natural power n
 
     Parameters:
-    a (float): The base number (real number).
-    n (int): The exponent (natural number, n >= 1).
+    a (float): The base number (real number)
+    n (int): The exponent (natural number, n >= 1)
 
     Returns:
-    float: a raised to the power n.
+    float: a raised to the power n
     """
     if n == 1:
         return a
