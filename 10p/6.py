1001bdf542cbe9e6f0fd232bbfde68c4bc57dac8
Maria Chepeleva <56710297+mrukly@users.noreply.github.com>
2026-05-23 15:25:34 +0700

    Update 6.py

--- 10p/6.py
+++ 10p/6.py
@@ -1,4 +1,13 @@
 def check(sms):
+    """
+    Returns the full message if length < 160, otherwise returns first 160 characters
+
+    Parameters:
+    sms (str): The input message
+
+    Returns:
+    str: Valid message
+    """
     if len(sms) <= 160:
         return sms
     else:
