71f9a0bdc3b3e6714017b07be66d82b9ce1c12aa
Maria Chepeleva <56710297+mrukly@users.noreply.github.com>
2026-05-23 15:12:04 +0700

    Update 2.py

--- 10p/2.py
+++ 10p/2.py
@@ -1,4 +1,14 @@
 def fibonacci(n): 
+    """
+    Prints the first N numbers of the Fibonacci sequence
+
+    Parameters:
+    n (int): The number of Fibonacci numbers to print (n >= 1)
+
+    Returns:
+    None
+    """
+    
     a, b = 1, 1
     
     for i in range(n):
